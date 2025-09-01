0da203dd0312feee3553850749420532491f8152
MuzammilGanihar1507 <muzammilganihar@gmail.com>
2025-09-01 11:32:55 +0530

    updated database(models.py) to use foreign key

--- ToDoApp/models.py
+++ ToDoApp/models.py
@@ -1,5 +1,19 @@
-from sqlalchemy import Boolean, Column, Integer, String
+from sqlalchemy import Boolean, Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 from database import Base
+
+class Users(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True, index=True)
+    email_id = Column(String, unique=True, index=True)
+    username = Column(String, unique=True, index=True)
+    first_name = Column(String)
+    last_name = Column(String)
+    hashed_password = Column(String)
+    is_active = Column(Boolean)
+
+    todos = relationship("Todos", backref="user")
 
 class TodoList(Base):
     __tablename__ = 'todo_list'
@@ -9,4 +23,7 @@ class TodoList(Base):
     description = Column(String)
     priority = Column(Integer)
     complete = Column(Boolean, default=False)
+    owner_id = Column(Integer, ForeignKey('users.id'))
+
+    owner = relationship("Users", back_populates="todos")
 
