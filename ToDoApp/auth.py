598b20e895d4ed456605e5ce578d7716303c7535
MuzammilGanihar1507 <muzammilganihar@gmail.com>
2025-09-01 22:34:54 +0530

    authentication of user-simple implementation.

--- ToDoApp/auth.py
+++ ToDoApp/auth.py
@@ -1,10 +1,11 @@
-from fastapi import FastAPI, Depends
+from fastapi import FastAPI, Depends, HTTPException
 from pydantic import BaseModel
 from typing import Optional
 import models
 from passlib.context import CryptContext
 from sqlalchemy.orm import Session
 from database import SessionLocal, engine
+from fastapi.security import OAuth2PasswordRequestForm
 
 class CreateUser(BaseModel):
     username: str
@@ -29,6 +30,18 @@ def get_db():
 def get_password_hash(password):
     return bcrypt_context.hash(password)
 
+def verify_password(plain_password, hashed_password):
+    return bcrypt_context.verify(plain_password, hashed_password)
+
+def authenticate_user(username: str, password: str, db):
+    user = db.query(models.Users).filter(models.Users.username == username).first()
+
+    if not user:
+        return False
+    if not verify_password(password, user.hashed_password):
+        return False
+    return user
+
 @app.post("/create/user")
 async def create_user(user_data: CreateUser, db: Session = Depends(get_db)):
     user_data_model = models.Users()
@@ -44,4 +57,10 @@ async def create_user(user_data: CreateUser, db: Session = Depends(get_db)):
 
     db.add(user_data_model)
     db.commit()
-    return user_data_model.email_id
+
+@app.post("/token")
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = authenticate_user(form_data.username, form_data.password, db)
+    if not user:
+        raise HTTPException(status_code=404, detail="Incorrect username or password")
+    return 'User Validated!'
